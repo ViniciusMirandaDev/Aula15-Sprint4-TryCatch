a64697a2801f6e36fce8e424657efaf33b2b865a
ViniciusMirandaDev <ViniciusMirandaDev>
2020-06-11 14:13:52 -0300

    Projeto Iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula15_Sprint4_TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
